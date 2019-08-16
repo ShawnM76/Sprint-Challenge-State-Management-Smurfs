7bb664e8e098545effeefcd72a41893ebd7c0e46
Shawn McManus <xsvfan76@gmail.com>
2019-08-16 13:20:25 -0600

    added-reducers-actions-and-finishing-components

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have funn!</div>
-      </div>
-    );
-  }
-}
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default App;
+import './App.css';
+const App = props => {
+  return (
+    <div className='App'>
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have funn!</div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    state,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {},
+  null,
+)(App);
